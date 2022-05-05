6850d5da9d31e421843a271b10d3df7128c58d17
Katsiaryna <katsiaryna.n@gmail.com>
2022-05-05 11:04:27 +0300

    init: start RSS VK Project

---
+++ index.js
@@ -0,0 +1,8 @@
+const wrapper = document.createElement('div');
+wrapper.className = 'wrapper';
+document.body.append(wrapper)
+
+const h1 = document.createElement('h1');
+h1.className = 'title';
+h1.innerHTML = 'RSS Виртуальная клавиатура'
+wrapper.append(h1)
